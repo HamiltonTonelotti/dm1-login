1ed680e7c27b53fc0fac89ac51738cd276a5f12d
HamiltonTonelotti <132910876+HamiltonTonelotti@users.noreply.github.com>
2024-08-19 22:26:09 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+// npx expo start (inicia o projeto para o seu celular)
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up jhghjghhjghg</Text>
       <StatusBar style="auto" />
     </View>
   );
